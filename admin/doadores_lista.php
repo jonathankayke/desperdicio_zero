25dfd65488d5084c6aee67a0f16729fb47028259
Mtths_nax <matheusnalesso9@gmail.com>
2025-11-18 21:46:35 -0300

    modified:   admin/doadores_lista.php

--- admin/doadores_lista.php
+++ admin/doadores_lista.php
@@ -1,36 +1,62 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-<div class="table-responsive">
-  <table class="table align-middle">
-    <thead>
-      <tr>
-        ...
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        ...
-      </tr>
-      <tr class="align-bottom">
-        ...
-      </tr>
-      <tr>
-        <td>...</td>
-        <td>...</td>
-        <td class="align-top">This cell is aligned to the top.</td>
-        <td>...</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
+<div class="container">
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
+<!-- tabela -->
+<div class="table-responsive">
+        <table class="table align-middle ">
+            <thead>
+                <tr>
+                    Lista de doadores
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <!-- php -->
+                </tr>
+                <tr class="align-bottom">
+                    <!-- php -->
+                </tr>
+                <tr>
+                    <td><!-- php -->.</td>
+                    <td><!-- php -->.</td>
+                    <td class="align-top"></td>
+                    <td><!-- php -->...</td>
+                </tr>
+              
+            </tbody>
+        </table>
+    </div> <!-- fecha tabela responsiva -->
+
+</div> <!-- fecha container -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
