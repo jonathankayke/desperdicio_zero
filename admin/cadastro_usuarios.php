4483fcddd3c3184f3a569127af56de225b0e8b83
vinicius <viniciusresende3486@gmail.com>
2025-11-18 22:05:20 -0300

    modified:   admin/cadastro_usuarios.php

--- admin/cadastro_usuarios.php
+++ admin/cadastro_usuarios.php
@@ -8,9 +8,14 @@
 </head>
 <body class="bg-light">
     <div class="container d-flex justify-content-center align-items-center" style="height: 100vh;">
-        <div class="form-control">
-            <span></span>
-            <div class="text-center">Cadastro Usuários</div>
+        <div class="card shadow border-0 p-4">
+            <div class="text-center mb-3">
+                <span class="font-size: 3rem;">🌿</span>
+                <h3 class="text-success fw-bold">Cadastrar</h3>
+                <form action="">
+                    
+                </form>
+            </div>
         </div>
     </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
