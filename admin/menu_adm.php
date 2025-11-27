15c663c71aa7566a86cd39c1850b6055a098dfa5
jonathankayke <jonathankayke663@gmail.com>
2025-11-26 21:59:05 -0300

    modified:   admin/menu_adm.php

--- admin/menu_adm.php
+++ admin/menu_adm.php
@@ -3,14 +3,42 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Modelo</title>
+    <title>Área Administrativa</title>
     <!-- Link CSS do Bootstrap -->
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <!-- Link para CSS Específico -->
     <link rel="stylesheet" href="../css/meu_estilo.css">
 </head>
 <body>
-
+    <nav class="navbar navbar-success">
+        <div class="container-fluid">
+    
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed"
+                    data-toggle="collapse"
+                    data-target="#defaultNavbar">
+                    <span class="sr-only">Navegação Mobile</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+    
+                <a href="index.php" class="navbar-brand">
+                    <img src="" alt="">
+                </a>
+            </div>
+    
+            <div class="collapse navbar-collapse" id="defaultNavbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="active"><a href="index.php">ADMIN</a></li>
+                    <li><a href="">DOAÇÕES</a></li>
+                    <li><a href="">TIPOS</a></li>
+                    <li><a href="">USUÁRIOS</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    
 <!-- Link arquivos Bootstrap js -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="../js/bootstrap.min.js"></script>    
