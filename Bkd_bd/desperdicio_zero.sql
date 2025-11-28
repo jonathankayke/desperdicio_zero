3225865231b52ac9ebb47585b8e36ffe3112fc28
gustavoalmeidacamargo16-maker <gustavoalmeidacamargo16@gmail.com>
2025-11-27 22:05:52 -0300

    modified:   Bkd_bd/desperdicio_zero.sql

--- Bkd_bd/desperdicio_zero.sql
+++ Bkd_bd/desperdicio_zero.sql
@@ -21,33 +21,14 @@ CREATE DATABASE IF NOT EXISTS desperdicio_zero
 -- Usamos o banco de dados desperdicio_zero
 USE desperdicio_zero;
 
--- Tabela de doadores
-CREATE TABLE doadores (
-    id_doador INT AUTO_INCREMENT PRIMARY KEY,
-    nome_doador VARCHAR(150) NOT NULL,
-    tipo_doador ENUM('Pessoa Física', 'Empresa') NOT NULL,
-    cpf_cnpj VARCHAR(20) NOT NULL UNIQUE,
-    email_doador VARCHAR(120) NOT NULL,
-    whatsapp_doador VARCHAR(20),
-    endereco_doador VARCHAR(255),
-    senha_doador VARCHAR(255) NOT NULL,  -- senha deve ser criptografada no PHP usando password_hash
-    criado_em_doador TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
- 
-INSERT INTO doadores (nome_doador, tipo_doador, cpf_cnpj, email_doador, whatsapp_doador, endereco_doador, senha_doador) VALUES
-('Mercadinho Bom Preço', 'Empresa', '12.345.678/0001-90', 'contato@bompreco.com', '(11) 95555-1234', 'Rua das Flores, 120 - Centro', '$2y$10$Z7d71rxeFA2CjqxJcw8oHeHqFQkQJJ0rNWIGQqfYTLsAECA30rFO2'),
-('Padaria Pão Doce', 'Empresa', '98.765.432/0001-11', 'padaria@paodoce.com', '(11) 97777-8888', 'Av. Principal, 450 - Jardim Alto', '$2y$10$Z7d71rxeFA2CjqxJcw8oHeHqFQkQJJ0rNWIGQqfYTLsAECA30rFO2'),
-('João da Silva', 'Pessoa Física', '123.456.789-01', 'joao@gmail.com', '(11) 94444-2222', 'Rua Nova Esperança, 88', '$2y$10$Z7d71rxeFA2CjqxJcw8oHeHqFQkQJJ0rNWIGQqfYTLsAECA30rFO2'),
-('Maria Oliveira', 'Pessoa Física', '987.654.321-00', 'maria.oliveira@yahoo.com', '(11) 93333-1111', 'Travessa da Paz, 15', '$2y$10$Z7d71rxeFA2CjqxJcw8oHeHqFQkQJJ0rNWIGQqfYTLsAECA30rFO2');
- 
-
 -- Tabela de usuários
-CREATE TABLE usuarios (
+CREATE TABLE tbusuarios (
     id_usuario INT AUTO_INCREMENT PRIMARY KEY,
     nome_usuario VARCHAR(150) NOT NULL,
     email_usuario VARCHAR(120) NOT NULL UNIQUE,
     senha_usuario VARCHAR(255) NOT NULL,   -- use password_hash no PHP
-    tipo_usuario ENUM('Admin', 'User') DEFAULT 'User'
+    tipo_usuario ENUM('Admin', 'User') DEFAULT 'User',
+    foto_usuario
 );
  
 INSERT INTO usuarios (nome_usuario, email_usuario, senha_usuario, tipo_usuario) VALUES
@@ -57,16 +38,16 @@ INSERT INTO usuarios (nome_usuario, email_usuario, senha_usuario, tipo_usuario) 
  
 
 -- Tabela de doações
-CREATE TABLE doacoes (
+CREATE TABLE tbdoacoes (
     id_doacao INT AUTO_INCREMENT PRIMARY KEY,
-    nome_alimento VARCHAR(150) NOT NULL,
+    tipo_alimento VARCHAR(150) NOT NULL,
     quantidade_doacao VARCHAR(50) NOT NULL,
     validade_doacao DATE,
     descricao_doacao TEXT,
     data_doacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
  
-INSERT INTO doacoes (nome_alimento, quantidade_doacao, validade_doacao, descricao_doacao)
+INSERT INTO doacoes (tipo_alimento, quantidade_doacao, validade_doacao, descricao_doacao)
 VALUES
 ('Arroz', '10 kg', '2025-05-10', 'Pacotes novos, ótima condição'),
 ('Feijão', '5 kg', '2025-04-22', 'Produto fechado, pronto para uso'),
@@ -74,72 +55,12 @@ VALUES
 ('Pão francês', '20 unidades', '2025-02-10', 'Pães feitos hoje'),
 ('Leite', '6 caixas', '2025-06-01', 'Validade longa');
 
-
--- Tabela de solicitações
-
-USE desperdicio_zero;
- 
-CREATE TABLE IF NOT EXISTS solicitacoes (
-    id_solicitacao INT AUTO_INCREMENT PRIMARY KEY,
-    id_doacao INT NOT NULL,
-    id_usuario INT NOT NULL,
-    status ENUM('pendente','aceita','retirada','cancelada') DEFAULT 'pendente',
-    data_solicitacao DATETIME DEFAULT CURRENT_TIMESTAMP,
- 
-    FOREIGN KEY (id_doacao) REFERENCES doacoes(id_doacao)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
- 
-    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-);
- 
-USE desperdicio_zero;
- 
-DELIMITER $$
- 
-CREATE TRIGGER trg_solicitacao_insert
-AFTER INSERT ON solicitacoes
-FOR EACH ROW
-BEGIN
-    UPDATE doacoes
-    SET status = 'reservado'
-    WHERE id_doacao = NEW.id_doacao;
-END$$
- 
-DELIMITER ;
- 
-DELIMITER $$
- 
-CREATE TRIGGER trg_solicitacao_retirada
-AFTER UPDATE ON solicitacoes
-FOR EACH ROW
-BEGIN
-    IF NEW.status = 'retirada' THEN
-        UPDATE doacoes
-        SET status = 'retirado'
-        WHERE id_doacao = NEW.id_doacao;
-    END IF;
-END$$
- 
-DELIMITER ;
- 
-DELIMITER $$
- 
-CREATE TRIGGER trg_solicitacao_cancelada
-AFTER UPDATE ON solicitacoes
-FOR EACH ROW
-BEGIN
-    IF NEW.status = 'cancelada' THEN
-        UPDATE doacoes
-        SET status = 'disponivel'
-        WHERE id_doacao = NEW.id_doacao;
-    END IF;
-END$$
- 
-DELIMITER ;
- 
+    -- Estrutura da tabela tbtipos
+    CREATE TABLE tbtipos(
+        id_tipo INT(11) NOT NULL,
+        sigla_tipo VARCHAR(3) NOT NULL,
+        rotulo_tipo VARCHAR(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 
