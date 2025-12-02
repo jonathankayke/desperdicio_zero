d14b5e1e3c92c725a7a5638ab12580f10358528f
vinicius <viniciusresende3486@gmail.com>
2025-12-01 21:05:40 -0300

    new file:   admin/doacao_lista.php

---
+++ admin/doacao_lista.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Doações</title>
+    <!-- Link CSS do Bootstrap -->
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <!-- Link para CSS Específico -->
+    <link rel="stylesheet" href="../css/meu_estilo.css">
+</head>
+<body class="fundofixo">
+    <main class="container">
+        <h1 class="text-center" style="color: white; text-shadow: 1px 1px 3px rgba(0,0,0,0.5); margin-bottom: 20px;">
+            Lista de Doações
+        </h1>
+        <table class="table table-hover table-condensed">
+            <thead>
+                <tr>
+                    <td>ID</td>
+                    <td>Nome Doador</td>
+                    <td>Alimento</td>
+                    <td>Quantidade</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </thead>
+        </table>
+
+    </main>
+<!-- Link arquivos Bootstrap js -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="../js/bootstrap.min.js"></script>    
+</body>
+</html>
