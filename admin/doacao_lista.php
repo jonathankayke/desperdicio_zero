98f3c46152e620f62076a4a012a82b60ac73da56
vinicius <viniciusresende3486@gmail.com>
2025-12-01 22:27:26 -0300

    modified:   admin/doacao_lista.php 	modified:   css/meu_estilo.css

--- admin/doacao_lista.php
+++ admin/doacao_lista.php
@@ -1,3 +1,12 @@
+<?php
+include("../Connections/conn_alimentos.php");
+
+$consulta   =   "
+                SELECT  *
+                FROM vw_doacoes
+                ORDER BY nome_alimento
+                ";
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -11,22 +20,34 @@
 </head>
 <body class="fundofixo">
     <main class="container">
-        <h1 class="text-center" style="color: white; text-shadow: 1px 1px 3px rgba(0,0,0,0.5); margin-bottom: 20px;">
-            Lista de Doações
-        </h1>
-        <table class="table table-hover table-condensed">
+        <h1 class="breadcrumb alert-success text-center">Lista de Doações</h1>
+        <div class="btn btn-success disabled">
+            Total de Produtos:
+        </div>
+        <table class="table table-hover table-condensed tbopacidade">
             <thead>
                 <tr>
-                    <td>ID</td>
-                    <td>Nome Doador</td>
-                    <td>Alimento</td>
-                    <td>Quantidade</td>
+                    <th>ID</th>
+                    <th>Instituição</th>
+                    <th>Alimento</th>
+                    <th>Quantidade</th>
+                    <th>Validade</th>
+                    <th>Endereço</th>
+                    <th>IMAGEM</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
                     <td></td>
                     <td></td>
                 </tr>
-            </thead>
+            </tbody>
         </table>
-
     </main>
 <!-- Link arquivos Bootstrap js -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
