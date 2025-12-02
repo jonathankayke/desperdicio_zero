4ffe03cb9c56f166a9796dc6cf96bbaf6b18cb3f
Mcirilo01 <paratestes571@gmail.com>
2025-12-02 19:56:44 -0300

    modified:   Connections/conn_alimentos.php

--- Connections/conn_alimentos.php
+++ Connections/conn_alimentos.php
@@ -1,16 +1,4 @@
-
 <?php
-$hostname_conn  =   "localhost";
-$database_conn  =   "desperdicio_zero";
-$username_conn  =   "desperdicio_zero";
-$password_conn  =   "1234";
-$charset_conn   =   "utf8";
-
-$conn_alimentos =   new mysqli($hostname_conn, $username_conn, $password_conn, $database_conn);
-
-mysqli_set_charset($conn_alimentos,$charset_conn);
-
-
 // Definindo variáveis para conexão
 $hostname_conn  =   "localhost";
 $database_conn  =   "desperdicio_zero";
