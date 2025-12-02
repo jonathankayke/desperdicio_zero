1d67b54596616dd3916a67c2c04b516c0a3dc4f4
gustavoalmeidacamargo16-maker <gustavoalmeidacamargo16@gmail.com>
2025-12-02 19:57:24 -0300

    modified:   admin/Doacao_atualiza.php

--- admin/Doacao_atualiza.php
+++ admin/Doacao_atualiza.php
@@ -145,6 +145,7 @@
     </div>
 </div>
 
-<script src="../js/bootstrap.bundle.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
 </body>
 </html>
