97cbed4e09427411dd37145b2b381df81a05dc33
vinicius <viniciusresende3486@gmail.com>
2025-12-03 20:21:25 -0300

    modified:   admin/doacao_lista.php 	modified:   css/meu_estilo.css

--- admin/doacao_lista.php
+++ admin/doacao_lista.php
@@ -21,43 +21,52 @@ $totalRows  =   ($lista)->num_rows;
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <!-- Link para CSS Específico -->
     <link rel="stylesheet" href="../css/meu_estilo.css">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet">
 </head>
 <body class="fundofixo">
     <main class="container">
-        <div class="col-xs-12 col-md-10 col-md-offset-1">
-            <h1 class="breadcrumb alert-success text-center">Lista de Doações</h1>
-            <div class="btn btn-success disabled">
-                Total de Produtos:
-            </div>
-            <table class="table table-hover table-condensed tbopacidade">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Instituição</th>
-                        <th>Alimento</th>
-                        <th>Quantidade</th>
-                        <th>Validade</th>
-                        <th>Endereço</th>
-                        <th>IMAGEM</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <!-- abre looping -->
-                    <?php do{ ?> 
-                    <tr>
-                        <td><?php echo $row['id_doacao']; ?></td>
-                        <td><?php echo $row['nome_doacao']?></td>
-                        <td><?php echo $row['nome_alimento']?></td>
-                        <td><?php echo $row['quantidade_doacao']?></td>
-                        <td><?php echo $row['validade_doacao']?></td>
-                        <td><?php echo $row['endereco_retirada']?></td>
-                        <td><?php echo $row['']?></td>
-                    </tr>
-                    <?php }while($row = $lista->fetch_assoc()); ?>
-                    <!-- fecha looping -->
-                </tbody>
-            </table>
+        <h1 class="breadcrumb alert-success text-center">Lista de Doações</h1>
+        <div class="btn btn-success disabled">
+            Total de Produtos:
         </div>
+        <table class="table table-hover table-condensed tbopacidade fontelista">
+            <thead>
+                <tr>
+                    <th class="hidden">ID</th>
+                    <th>Instituição</th>
+                    <th>Alimento</th>
+                    <th>Quantidade</th>
+                    <th>Validade</th>
+                    <th>Endereço</th>
+                    <th>IMAGEM</th>
+                    <th>
+                    <a href="Doacao_insere.php" class="btn btn-block btn-success btn-xs">
+                        <span>ADICIONAR<br> </span>
+                        <span class="glyphicon glyphicon-plus"></span>
+                    </a> 
+                    </th>
+
+                </tr>
+            </thead>
+            <tbody>
+                <!-- abre looping -->
+                <?php do{ ?> 
+                <tr>
+                    <td class="hidden"><?php echo $row['id_doacao']; ?></td>
+                    <td><?php echo $row['nome_doacao']?></td>
+                    <td><?php echo $row['nome_alimento']?></td>
+                    <td><?php echo $row['quantidade_doacao']?></td>
+                    <td><?php echo $row['validade_doacao']?></td>
+                    <td><?php echo $row['endereco_retirada']?></td>
+                    <td>
+                        <img src="../imagens/<?php echo $row['imagem_doacao']; ?>" alt="" width="100
+                        px">
+                    </td>
+                </tr>
+                <?php }while($row = $lista->fetch_assoc()); ?>
+                <!-- fecha looping -->
+            </tbody>
+        </table>
     </main>
 <!-- Link arquivos Bootstrap js -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
