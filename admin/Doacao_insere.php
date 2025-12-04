ef64ae9b9c93d093384e0a723ede2f31b1a2b24e
Mtths_nax <matheusnalesso9@gmail.com>
2025-12-04 20:15:58 -0300

    modified:   admin/Doacao_insere.php

--- admin/Doacao_insere.php
+++ admin/Doacao_insere.php
@@ -4,118 +4,74 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro Doações</title>
+    <title>Cadastro de Usuários</title>
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/meu_estilo.css">
-
 </head>
-<?php include("menu_adm.php") ?>
+
 <body class="fundofixo">
 
+    <?php include("menu_adm.php"); ?>
 
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-md-10 col-md-offset-1">
 
-                <h1 class="text-center"
-                    style="color: white; text-shadow: 1px 1px 3px rgba(0,0,0,0.5); margin-bottom: 20px;">
-                    Cadastro
+                <h1 class="text-center" style="color: white; text-shadow: 1px 1px 3px rgba(0,0,0,0.5); margin-bottom: 20px;">
+                    Cadastro de Usuários
                 </h1>
 
                 <div class="thumbnail" style="border: none; background: none; box-shadow: none;">
-
                     <div class="alert alert-success" role="alert">
-                        <form action="processar_cadastro.php" method="POST">
+                        
+                        <form action="usuario_insere.php" method="POST" name="form_usuario" id="form_usuario">
 
                             <div class="row">
-                                <div class="col-md-6">
-                                    <h3 class="secao-titulo">Dados do Doador</h3>
+                                <div class="col-md-12">
+                                    <h3 class="secao-titulo">Dados Pessoais</h3>
 
                                     <div class="form-group">
-                                        <label for="nome_doador" class="form-label">Seu Nome/Nome da Empresa</label>
-                                        <input type="text" class="form-control" id="nome_doador" name="nome_doador"
-                                            placeholder="Ex: Restaurante Sabor da Casa" required>
+                                        <label for="nome_usuario" class="form-label">Nome Completo</label>
+                                        <input type="text" class="form-control" id="nome_usuario" name="nome_usuario"
+                                            placeholder="Digite o nome completo" required>
                                     </div>
 
                                     <div class="form-group">
-                                        <label for="tipo_instituicao" class="form-label">Tipo de Instituição</label>
-                                        <input type="text" class="form-control" id="tipo_instituicao"
-                                            name="tipo_instituicao" placeholder="Ex: Mercado, Restaurante, Igreja, etc."
-                                            required>
+                                        <label for="email_usuario" class="form-label">E-mail</label>
+                                        <input type="email" class="form-control" id="email_usuario" name="email_usuario"
+                                            placeholder="seuemail@exemplo.com" required>
                                     </div>
-
                                     
-
                                     <div class="form-group">
-                                        <label for="whatsapp" class="form-label">Contato (WhatsApp)</label>
-                                        <input type="tel" class="form-control" id="whatsapp" name="whatsapp"
-                                            placeholder="(XX) 9XXXX-XXXX" required>
+                                        <label for="fone_usuario" class="form-label">Telefone (Opcional)</label>
+                                        <input type="tel" class="form-control" id="fone_usuario" name="fone_usuario"
+                                            placeholder="(XX) 9XXXX-XXXX">
                                     </div>
                                 </div>
 
-                                <div class="col-md-6">
-                                    <h3 class="secao-titulo">Dados da Doação</h3>
+                                <div class="col-md-12" style="margin-top: 30px;">
+                                    <h3 class="secao-titulo">Dados de Acesso</h3>
 
                                     <div class="form-group">
-                                        <label for="tipo_alimento" class="form-label">Tipo de Alimento</label>
-                                        <select class="form-select form-control" id="tipo_alimento" name="tipo_alimento"
-                                            required>
-                                            <option value="" selected disabled>Selecione uma categoria</option>
-                                            <option value="#"><!-- coloque o php para mostrar as opçao --></option>
-                                          
+                                        <label for="login_usuario" class="form-label">Login (Nome de Usuário)</label>
+                                        <input type="text" class="form-control" id="login_usuario" name="login_usuario"
+                                            placeholder="Ex: admin.sistema" required>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="senha_usuario" class="form-label">Senha</label>
+                                        <input type="password" class="form-control" id="senha_usuario" name="senha_usuario"
+                                            placeholder="Digite uma senha segura" required>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="nivel_usuario" class="form-label">Nível de Acesso</label>
+                                        <select class="form-control" name="nivel_usuario" id="nivel_usuario" required>
+                                            <option value="" disabled selected>Selecione o nível</option>
+                                            <option value="sup">Supervisor</option>
+                                            <option value="com">Comum</option>
                                         </select>
                                     </div>
-
-                                    <div class="form-group">
-                                        <label for="alimento_especifico" class="form-label">Alimento Específico</label>
-                                        <input type="text" class="form-control" id="alimento_especifico"
-                                            name="alimento_especifico" placeholder="Ex: Maçã, Arroz, Pão Francês"
-                                            required>
-                                    </div>
-
-                                    <div class="row">
-                                        <div class="col-md-6 form-group">
-                                            <label for="quantidade" class="form-label">Quantidade</label>
-                                            <input type="text" class="form-control" id="quantidade" name="quantidade"
-                                                placeholder="Ex: 5 kg" required>
-                                        </div>
-                                        <div class="col-md-6 form-group">
-                                            <label for="validade" class="form-label">Validade</label>
-                                            <input type="date" class="form-control" id="validade" name="validade"
-                                                required>
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group">
-                                        <label for="endereco" class="form-label">Endereço para Retirada</label>
-                                        <input type="text" class="form-control" id="endereco" name="endereco"
-                                            placeholder="Rua, Número, Bairro, Cidade" required>
-                                    </div>
-
-                                    <div class=" form-group">
-                                    <label for="imagem_produto">Imagem</label>
-                                    
-                                        <span class="input-group-addon">
-                                            <span class="glyphicon glyphicon-picture"></span>
-                                        </span>
-                                        <!-- Exibir a imagem a ser inserida -->
-                                        <img 
-                                            src="" 
-                                            alt=""
-                                            name="imagem"
-                                            id="imagem"
-                                            class="img-responsive"
-                                            style="max-height: 500px;"
-                                        >
-                                        <input 
-                                            type="file" 
-                                            name="imagem_produto" 
-                                            id="imagem_produto"
-                                            class="form-control"
-                                            accept="image/*"
-                                        >
-                                    </div> 
-                                   
                                 </div>
                             </div>
 
@@ -123,10 +79,11 @@
                                 <div class="col-md-12 text-center">
                                     <br>
                                     <button type="submit" class="btn btn-success btn-lg btn-block">
-                                        Salvar Doação
+                                        <span class="glyphicon glyphicon-save"></span> Cadastrar Usuário
                                     </button>
                                 </div>
                             </div>
+
                         </form>
                     </div>
                 </div>
@@ -134,37 +91,6 @@
         </div>
     </div>
 
-    <?php include("../rodape.php")?>
-
-    <!-- Script para a imagem -->
-    <script>
-        document.getElementById("imagem_produto").onchange = function(){
-            var reader = new FileReader();
-            if(this.files[0].size>512000){
-                alert("A imagem deve ter no máximo 500kb.");
-                $("#imagem").attr("src","blank");
-                $("#imagem").hide();
-                $("#imagem_produto").wrap('<form>').closest('form').get(0).reset();
-                $("#imagem_produto").unwrap();
-                return false;
-            }
-            if(this.files[0].type.indexOf("image")==-1){
-                alert("Formato inválido, escolha uma imagem.");
-                $("#imagem").attr("src","blank");
-                $("#imagem").hide();
-                $("#imagem_produto").wrap('<form>').closest('form').get(0).reset();
-                $("#imagem_produto").unwrap();
-                return false;
-            }
-            reader.onload = function(e){
-                // obter dados carregados e renderizar uma miniatura.
-                document.getElementById("imagem").src = e.target.result;
-                $("imagem").show();
-            };
-        // leia o arquivo de imagem como um URL de dados.
-        reader.readAsDataURL(this.files[0]);
-        };
-</script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="../js/bootstrap.min.js"></script>
 </body>
