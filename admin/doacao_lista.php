8be0e074f32e9d31fa7614c03181d630227272a7
vinicius <viniciusresende3486@gmail.com>
2025-12-04 20:52:35 -0300

    modified:   admin/doacao_lista.php

--- admin/doacao_lista.php
+++ admin/doacao_lista.php
@@ -34,8 +34,8 @@ $totalRows  =   ($lista)->num_rows;
             <thead>
                 <tr>
                     <th class="hidden">ID</th>
+                    <th>Nome empresa</th>
                     <th>Instituição</th>
-                    <th>Doação</th>
                     <th>Alimento</th>
                     <th>Quantidade</th>
                     <th>Validade</th>
@@ -52,11 +52,11 @@ $totalRows  =   ($lista)->num_rows;
             </thead>
             <tbody>
                 <!-- abre looping -->
-                <?php do{ ?> 
+                <?php while($row = $lista->fetch_assoc()) { ?> 
                 <tr>
                     <td class="hidden"><?php echo $row['id_doacao']; ?></td>
+                    <td><?php echo $row['nome_empresa']?></td>
                     <td><?php echo $row['tipo_instituicao']?></td>
-                    <td><?php echo $row['nome_doacao']?></td>
                     <td><?php echo $row['nome_alimento']?></td>
                     <td><?php echo $row['quantidade_doacao']?></td>
                     <td><?php echo $row['validade_doacao']?></td>
@@ -75,7 +75,7 @@ $totalRows  =   ($lista)->num_rows;
                         </button>
                     </td>
                 </tr>
-                <?php }while($row = $lista->fetch_assoc()); ?>
+                <?php } ?>
                 <!-- fecha looping -->
             </tbody>
         </table>
