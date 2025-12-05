ca79de3be442cdc6092803b3ddb0b8598dc6d7a3
gustavoalmeidacamargo16-maker <gustavoalmeidacamargo16@gmail.com>
2025-12-04 21:01:07 -0300

    modified:   admin/usuario_atualiza.php

--- admin/usuario_atualiza.php
+++ admin/usuario_atualiza.php
@@ -17,58 +17,58 @@
         <div class="row">
             <div class="col-xs-12 col-md-10 col-md-offset-1">
 
-                <h1 class="text-center" style="color: white; text-shadow: 1px 1px 3px rgba(0,0,0,0.5); margin-bottom: 20px;">
-                    Atualização de Usuários
+                <h1 class="text-center" style="color:white; text-shadow:1px 1px 3px rgba(0,0,0,0.5); margin-bottom:20px;">
+                    Atualização de Usuário
                 </h1>
 
-                <div class="thumbnail" style="border: none; background: none; box-shadow: none;">
+                <div class="thumbnail" style="border:none; background:none;">
                     <div class="alert alert-success" role="alert">
-                        
-                        <form action="usuario_atualiza.php" method="POST">
+
+                        <form action="usuario_atualiza.php" method="POST" enctype="multipart/form-data">
 
                             <!-- ID oculto -->
-                            <input type="hidden" name="id_usuario" value="<?php echo $row_Usuario['id_usuario']; ?>">
+                            <input type="hidden" name="id_usuario" value="<?=$dados['id_usuario']?>">
 
                             <div class="row">
-                                <div class="col-md-12">
+                                <div class="col-md-6">
                                     <h3 class="secao-titulo">Dados Pessoais</h3>
 
                                     <div class="form-group">
                                         <label for="nome_usuario">Nome Completo</label>
                                         <input type="text" class="form-control" id="nome_usuario" 
-                                            name="nome_usuario" required
-                                            >
+                                               name="nome_usuario" required
+                                               value="">
                                     </div>
 
                                     <div class="form-group">
                                         <label for="email_usuario">E-mail</label>
-                                        <input type="email" class="form-control" id="email_usuario"
-                                            name="email_usuario" required
-                                            >
+                                        <input type="email" class="form-control" id="email_usuario" 
+                                               name="email_usuario" required
+                                               value="">
                                     </div>
 
                                     <div class="form-group">
-                                        <label for="fone_usuario">Telefone (Opcional)</label>
-                                        <input type="tel" class="form-control" id="fone_usuario"
-                                            name="fone_usuario"
-                                            >
+                                        <label for="fone_usuario">Telefone</label>
+                                        <input type="tel" class="form-control" id="fone_usuario" 
+                                               name="fone_usuario"
+                                               value="">
                                     </div>
                                 </div>
 
-                                <div class="col-md-12" style="margin-top: 30px;">
+                                <div class="col-md-6">
                                     <h3 class="secao-titulo">Dados de Acesso</h3>
 
                                     <div class="form-group">
-                                        <label for="login_usuario">Login (Nome de Usuário)</label>
-                                        <input type="text" class="form-control" id="login_usuario"
-                                            name="login_usuario" required
-                                            >
+                                        <label for="login_usuario">Login</label>
+                                        <input type="text" class="form-control" id="login_usuario" 
+                                               name="login_usuario" required
+                                               value="">
                                     </div>
 
                                     <div class="form-group">
-                                        <label for="senha_usuario">Senha (deixe em branco para manter)</label>
-                                        <input type="password" class="form-control" id="senha_usuario"
-                                            name="senha_usuario">
+                                        <label for="senha_usuario">Senha (deixe em branco para manter a atual)</label>
+                                        <input type="password" class="form-control" id="senha_usuario" 
+                                               name="senha_usuario" placeholder="Digite apenas se quiser alterar">
                                     </div>
 
                                     <div class="form-group">
@@ -78,6 +78,23 @@
                                             <option value="com" >Comum</option>
                                         </select>
                                     </div>
+                                </div>
+
+                                <div class="form-group col-md-12">
+                                    <label>Imagem Atual</label><br>
+                                    
+                                    <img src="../imagens/usuarios/<?=$dados['imagem']?>"
+                                         class="img-responsive"
+                                         style="max-height: 250px; border:2px solid #ccc;">
+
+                                    <br><br>
+
+                                    <label for="imagem_produto">Nova Imagem (opcional)</label>
+                                    <input type="file" name="imagem_produto" id="imagem_produto" 
+                                           class="form-control" accept="image/*">
+
+                                    <img id="imagem" src="" alt="" class="img-responsive" 
+                                         style="max-height: 250px; margin-top:15px; display:none;">
                                 </div>
                             </div>
 
@@ -91,6 +108,7 @@
                             </div>
 
                         </form>
+
                     </div>
                 </div>
 
@@ -99,45 +117,34 @@
     </div>
 
 
-    <!-- Script para pré-visualização e validação da imagem -->
-<script>
-document.getElementById("imagem_produto").onchange = function() {
+    <!-- Script da imagem -->
+    <script>
+        document.getElementById("imagem_produto").onchange = function () {
+            var reader = new FileReader();
 
-    var file = this.files[0];
-    var reader = new FileReader();
+            if (this.files[0].size > 512000) {
+                alert("A imagem deve ter no máximo 500kb.");
+                $("#imagem").hide().attr("src", "");
+                this.value = "";
+                return false;
+            }
 
-    // Se nada foi selecionado
-    if (!file) return;
+            if (this.files[0].type.indexOf("image") === -1) {
+                alert("Formato inválido, escolha uma imagem.");
+                $("#imagem").hide().attr("src", "");
+                this.value = "";
+                return false;
+            }
 
-    // Validar tamanho (máx 500kb)
-    if (file.size > 512000) {
-        alert("A imagem deve ter no máximo 500kb.");
-        resetarImagem();
-        return false;
-    }
+            reader.onload = function (e) {
+                document.getElementById("imagem").src = e.target.result;
+                $("#imagem").show();
+            };
 
-    // Validar tipo
-    if (file.type.indexOf("image") === -1) {
-        alert("Formato inválido, escolha uma imagem.");
-        resetarImagem();
-        return false;
-    }
+            reader.readAsDataURL(this.files[0]);
+        };
+    </script>
 
-    // Mostrar pré-visualização
-    reader.onload = function(e) {
-        document.getElementById("imagem").src = e.target.result;
-        $("#imagem").show();
-    };
-
-    reader.readAsDataURL(file);
-};
-
-// Função para resetar input de arquivo e imagem
-function resetarImagem() {
-    $("#imagem").attr("src", "").hide();
-    $("#imagem_produto").val("");
-}
-</script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="../js/bootstrap.min.js"></script>
 
