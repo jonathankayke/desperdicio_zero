8be2fb47d46eb8da8b1b4acde537f061d000b360
Mtths_nax <matheusnalesso9@gmail.com>
2026-01-27 00:26:00 -0300

    modified:   Bkd_bd/desperdicio_zero.sql 	modified:   admin/Doacao_insere.php

--- admin/Doacao_insere.php
+++ admin/Doacao_insere.php
@@ -1,9 +1,10 @@
-<?php 
+<?php
+// Incluir o arquivo e fazer a conexão
 include("../Connections/conn_alimentos.php");
 
 if($_POST){
     // Selecionar o banco de dados (USE)
-    mysqli_select_db($conn_alimentos,$database_conn);
+    mysqli_select_db($conn_alimentos, $database_conn);
 
     // Variáveis para acrescentar dados no banco
     $tabela_insert  =   "tbdoacoes";
@@ -13,7 +14,6 @@ if($_POST){
                             tipo_instituicao,
                             contato_doacao,
                             tipo_alimento,
-                            imagem_produto,
                             nome_alimento,
                             quantidade_doacao,
                             validade_doacao,
@@ -30,26 +30,34 @@ if($_POST){
     };
 
     // Receber os dados do formulário
-    // Organizar os campos na mesma ordem
     $id_doacao_tipo     =   $_POST['id_doacao_tipo'];
     $nome_empresa       =   $_POST['nome_empresa'];
     $tipo_instituicao   =   $_POST['tipo_instituicao'];
-    $contato_doacao     =   $_POST['contato_doacao'];     
-    $tipo_alimento      =   $_POST['tipo_alimento'];
+    $contato_doacao     =   $_POST['contato_doacao'];
     $nome_alimento      =   $_POST['nome_alimento'];
     $quantidade_doacao  =   $_POST['quantidade_doacao'];
     $validade_doacao    =   $_POST['validade_doacao'];
     $endereco_retirada  =   $_POST['endereco_retirada'];
-    $imagem_produto     =   $_FILES['imagem_doacao']['name'];
+    $imagem_doacao      =   $_FILES['imagem_doacao']['name'];
+
+    // Lógica Extra: Buscar a SIGLA (F, V, G) pois o banco pede id E sigla
+    $query_sigla    = "SELECT sigla_tipo FROM tbtipos WHERE id_tipo = $id_doacao_tipo";
+    $result_sigla   = $conn_alimentos->query($query_sigla);
+    $row_sigla      = $result_sigla->fetch_assoc();
+    $tipo_alimento  = $row_sigla['sigla_tipo'];
 
     // Reunir os valores a serem inseridos
     $valores_insert =   "
-                        '$id_tipo_produto',
-                        '$destaque_produto',
-                        '$descri_produto',
-                        '$resumo_produto',
-                        '$valor_produto',
-                        '$imagem_produto'
+                            '$id_doacao_tipo',
+                            '$nome_empresa',
+                            '$tipo_instituicao',
+                            '$contato_doacao',
+                            '$tipo_alimento',
+                            '$nome_alimento',
+                            '$quantidade_doacao',
+                            '$validade_doacao',
+                            '$endereco_retirada',
+                            '$imagem_doacao'
                         ";
 
     // Consulta SQL para inserção dos dados
@@ -62,7 +70,7 @@ if($_POST){
     $resultado  =   $conn_alimentos->query($insertSQL);
 
     // Após a ação a página será redirecionada
-    $destino    =   "produtos_lista.php";
+    $destino    =   "doacao_lista.php";
     if(mysqli_insert_id($conn_alimentos)){
         header("Location: $destino");
     }else{
@@ -73,7 +81,7 @@ if($_POST){
 // Selecionar o banco de dados (USE)
 mysqli_select_db($conn_alimentos,$database_conn);
 
-// Selecionar os dados da chave estrangeira
+// Selecionar os dados da chave estrangeira (para o Select do formulário)
 $tabela_fk      =   "tbtipos";
 $ordenar_por    =   "rotulo_tipo ASC";
 $consulta_fk    =   "
@@ -94,49 +102,44 @@ $totalRows_fk   =   ($lista_fk)->num_rows;
     <title>Cadastro Doações</title>
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/meu_estilo.css">
-
 </head>
+
 <?php include("menu_adm.php") ?>
+
 <body class="fundofixo">
-
-
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-md-10 col-md-offset-1">
-
-                <h1 class="text-center"
-                    style="color: white; text-shadow: 1px 1px 3px rgba(0,0,0,0.5); margin-bottom: 20px;">
-                    Cadastro
+                <h1 class="text-center" style="color: white; text-shadow: 1px 1px 3px rgba(0,0,0,0.5); margin-bottom: 20px;">
+                    Cadastro de Doação
                 </h1>
 
                 <div class="thumbnail" style="border: none; background: none; box-shadow: none;">
-
                     <div class="alert alert-success" role="alert">
-                        <form action="processar_cadastro.php" method="POST">
+                        <form action="" method="POST" enctype="multipart/form-data">
 
                             <div class="row">
                                 <div class="col-md-6">
                                     <h3 class="secao-titulo">Dados do Doador</h3>
 
                                     <div class="form-group">
-                                        <label for="nome_doador" class="form-label">Seu Nome/Nome da Empresa</label>
-                                        <input type="text" class="form-control" id="nome_doador" name="nome_doador"
-                                            placeholder="Ex: Restaurante Sabor da Casa" required>
+                                        <label for="nome_empresa" class="form-label">Nome da Empresa/Doador</label>
+                                        <input type="text" class="form-control" id="nome_empresa" name="nome_empresa" placeholder="Ex: Restaurante Sabor" required>
                                     </div>
 
                                     <div class="form-group">
                                         <label for="tipo_instituicao" class="form-label">Tipo de Instituição</label>
-                                        <input type="text" class="form-control" id="tipo_instituicao"
-                                            name="tipo_instituicao" placeholder="Ex: Mercado, Restaurante, Igreja, etc."
-                                            required>
+                                        <input type="text" class="form-control" id="tipo_instituicao" name="tipo_instituicao" placeholder="Ex: Mercado, Restaurante" required>
                                     </div>
 
-                                    
-
                                     <div class="form-group">
-                                        <label for="whatsapp" class="form-label">Contato (WhatsApp)</label>
-                                        <input type="tel" class="form-control" id="whatsapp" name="whatsapp"
-                                            placeholder="(XX) 9XXXX-XXXX" required>
+                                        <label for="contato_doacao" class="form-label">Contato (WhatsApp)</label>
+                                        <input type="tel" class="form-control" id="contato_doacao" name="contato_doacao" placeholder="(XX) 9XXXX-XXXX" required>
+                                    </div>
+                                    
+                                    <div class="form-group">
+                                        <label for="endereco_retirada" class="form-label">Endereço para Retirada</label>
+                                        <input type="text" class="form-control" id="endereco_retirada" name="endereco_retirada" placeholder="Rua, Número, Bairro" required>
                                     </div>
                                 </div>
 
@@ -144,112 +147,62 @@ $totalRows_fk   =   ($lista_fk)->num_rows;
                                     <h3 class="secao-titulo">Dados da Doação</h3>
 
                                     <div class="form-group">
-                                        <label for="tipo_alimento" class="form-label">Tipo de Alimento</label>
-                                        <select class="form-select form-control" id="tipo_alimento" name="tipo_alimento"
-                                            required>
+                                        <label for="id_doacao_tipo" class="form-label">Categoria do Alimento</label>
+                                        <select class="form-select form-control" id="id_doacao_tipo" name="id_doacao_tipo" required>
                                             <option value="" selected disabled>Selecione uma categoria</option>
-                                            <option value="#"><!-- coloque o php para mostrar as opçao --></option>
-                                          
+                                            <?php do { ?>
+                                                <option value="<?php echo $row_fk['id_tipo']; ?>">
+                                                    <?php echo $row_fk['rotulo_tipo']; ?>
+                                                </option>
+                                            <?php } while ($row_fk = $lista_fk->fetch_assoc()); ?>
                                         </select>
                                     </div>
 
                                     <div class="form-group">
-                                        <label for="alimento_especifico" class="form-label">Alimento Específico</label>
-                                        <input type="text" class="form-control" id="alimento_especifico"
-                                            name="alimento_especifico" placeholder="Ex: Maçã, Arroz, Pão Francês"
-                                            required>
+                                        <label for="nome_alimento" class="form-label">Nome do Alimento</label>
+                                        <input type="text" class="form-control" id="nome_alimento" name="nome_alimento" placeholder="Ex: Maçã Fuji, Pão Francês" required>
                                     </div>
 
                                     <div class="row">
                                         <div class="col-md-6 form-group">
-                                            <label for="quantidade" class="form-label">Quantidade</label>
-                                            <input type="text" class="form-control" id="quantidade" name="quantidade"
-                                                placeholder="Ex: 5 kg" required>
+                                            <label for="quantidade_doacao" class="form-label">Quantidade</label>
+                                            <input type="text" class="form-control" id="quantidade_doacao" name="quantidade_doacao" placeholder="Ex: 5 kg" required>
                                         </div>
                                         <div class="col-md-6 form-group">
-                                            <label for="validade" class="form-label">Validade</label>
-                                            <input type="date" class="form-control" id="validade" name="validade"
-                                                required>
+                                            <label for="validade_doacao" class="form-label">Validade</label>
+                                            <input type="date" class="form-control" id="validade_doacao" name="validade_doacao" required>
                                         </div>
                                     </div>
 
                                     <div class="form-group">
-                                        <label for="endereco" class="form-label">Endereço para Retirada</label>
-                                        <input type="text" class="form-control" id="endereco" name="endereco"
-                                            placeholder="Rua, Número, Bairro, Cidade" required>
-                                    </div>
-
-                                    <div class=" form-group">
-                                    <label for="imagem_produto">Imagem</label>
-                                    
-                                        <span class="input-group-addon">
-                                            <span class="glyphicon glyphicon-picture"></span>
-                                        </span>
-                                        <!-- Exibir a imagem a ser inserida -->
-                                        <img 
-                                            src="" 
-                                            alt=""
-                                            name="imagem"
-                                            id="imagem"
-                                            class="img-responsive"
-                                            style="max-height: 500px;"
-                                        >
-                                        <input 
-                                            type="file" 
-                                            name="imagem_produto" 
-                                            id="imagem_produto"
-                                            class="form-control"
-                                            accept="image/*"
-                                        >
+                                        <label for="imagem_doacao">Foto da Doação</label>
+                                        <input type="file" name="imagem_doacao" id="imagem_doacao" class="form-control" accept="image/*">
+                                        
+                                        <img src="" alt="" id="imagem_preview" class="img-responsive" style="max-height: 200px; display:none; margin-top: 10px;">
                                     </div> 
-                                   
                                 </div>
                             </div>
 
-                            <input 
-                            type="submit" 
-                            value="Salvar Doação"
-                            name="enviar"
-                            id="enviar"
-                            class="btn btn-success btn-block"
-                         >
+                            <input type="submit" value="Salvar Doação" name="enviar" class="btn btn-success btn-block" style="margin-top: 20px;">
                         </form>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <!-- Script para a imagem -->
-    <script>
-        document.getElementById("imagem_produto").onchange = function(){
-            var reader = new FileReader();
-            if(this.files[0].size>512000){
-                alert("A imagem deve ter no máximo 500kb.");
-                $("#imagem").attr("src","blank");
-                $("#imagem").hide();
-                $("#imagem_produto").wrap('<form>').closest('form').get(0).reset();
-                $("#imagem_produto").unwrap();
-                return false;
-            }
-            if(this.files[0].type.indexOf("image")==-1){
-                alert("Formato inválido, escolha uma imagem.");
-                $("#imagem").attr("src","blank");
-                $("#imagem").hide();
-                $("#imagem_produto").wrap('<form>').closest('form').get(0).reset();
-                $("#imagem_produto").unwrap();
-                return false;
-            }
-            reader.onload = function(e){
-                // obter dados carregados e renderizar uma miniatura.
-                document.getElementById("imagem").src = e.target.result;
-                $("imagem").show();
-            };
-        // leia o arquivo de imagem como um URL de dados.
-        reader.readAsDataURL(this.files[0]);
-        };
-</script>
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="../js/bootstrap.min.js"></script>
+    
+    <script>
+        document.getElementById("imagem_doacao").onchange = function() {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                document.getElementById("imagem_preview").src = e.target.result;
+                $("#imagem_preview").show();
+            };
+            reader.readAsDataURL(this.files[0]);
+        };
+    </script>
 </body>
-
 </html>
