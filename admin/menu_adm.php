abb4fb0721b4f0e24f2fb94f93e7ef1260650f46
Mcirilo01 <paratestes571@gmail.com>
2026-01-28 20:35:17 -0300

    modified:   admin/menu_adm.php

--- admin/menu_adm.php
+++ admin/menu_adm.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="../css/meu_estilo.css">-->
 </head>
 <body>
-    <nav class="navbar navbar-inverse">
+    <nav class="navbar navegacao">
         <div class="container-fluid">
     
             <div class="navbar-header">
