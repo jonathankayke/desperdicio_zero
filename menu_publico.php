6bda4205e43de44e7758c07aa7f3be8e581da113
Mtths_nax <matheusnalesso9@gmail.com>
2026-01-29 19:35:56 -0300

    modified:   menu_publico.php

--- menu_publico.php
+++ menu_publico.php
@@ -14,3 +14,17 @@ $lista_menu     =   $conn_alimentos->query($consulta_menu);
 $row_menu       =   $lista_menu->fetch_assoc();
 $totalRows_menu =   ($lista_menu)->num_rows;
 ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Pública</title>
+    <!-- Link CSS do Bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Link para CSS Específico -->
+    <link rel="stylesheet" href="css/meu_estilo.css">
+</head>
+<body>
+    <? include("includes/menu_publico.php"); ?>
+</body>
