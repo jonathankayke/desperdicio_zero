03fe55e14a3c9ae63f70be2a4ba7ef788228b60e
jonathankayke <jonathankayke663@gmail.com>
2026-01-29 20:51:13 -0300

    new file:   admin2.php 	new file:   doacao_geral,php 	deleted:    index.php 	modified:   admin2.php 	modified:   doacao_geral,php

---
+++ admin2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desperdicio Zero</title>
+</head>
+<body class="fundofixo">
+    <!-- MENU -->
+    <a name="home"></a>
+    <?php include('menu_publico.php'); ?>
+
+    <main class="container">
+        
+        <!-- RODAPÉ -->
+        <footer>
+            <?php include('rodape.php'); ?>
+            <a name="contato"></a>
+        </footer>
+    </main>
+</body>
+</html>
