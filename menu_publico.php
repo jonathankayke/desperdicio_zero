3f9668e8cae6c5b090cfbf7fa6c3b993d5f2807b
Mtths_nax <matheusnalesso9@gmail.com>
2026-01-29 19:42:02 -0300

    modified:   menu_publico.php

--- menu_publico.php
+++ menu_publico.php
@@ -26,5 +26,5 @@ $totalRows_menu =   ($lista_menu)->num_rows;
     <link rel="stylesheet" href="css/meu_estilo.css">
 </head>
 <body>
-    <? include('admin/menu_admin.php'); ?>
+    <?php include('admin/menu_admin.php'); ?>
 </body>
