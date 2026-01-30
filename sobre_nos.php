3b661fffe1bad134fb8946076ab4d201fe0cc009
vinicius <viniciusresende3486@gmail.com>
2026-01-29 21:54:37 -0300

    modified:   css/meu_estilo.css 	new file:   imagens/cooking.png

---
+++ sobre_nos.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sobre nós - Desperdício Zero</title>
+    <!-- Link CSS do Bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Link para CSS Específico -->
+    <link rel="stylesheet" href="css/meu_estilo.css">
+</head>
+<body class="corfundo">
+    <main class="container">
+        <div class="text-center">
+            <h1>QUEM SOMOS?</h1>
+        </div>
+        <img src="imagens/cooking.png" alt="" class="img-rounded img-responsive">
+    </main>
+<!-- Link arquivos Bootstrap js -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>    
+</body>
+</html>
