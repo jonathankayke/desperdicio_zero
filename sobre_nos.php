e05a9b76040d888e34eef96957ff46c540fb3d60
vinicius <viniciusresende3486@gmail.com>
2026-01-30 19:18:46 -0300

    modified:   sobre_nos.php

--- sobre_nos.php
+++ sobre_nos.php
@@ -9,47 +9,10 @@
     <!-- Link para CSS Específico -->
     <link rel="stylesheet" href="css/meu_estilo.css">
 </head>
-<body class="corfundo">
+<body class="fundofixo">
     <main class="container">
-        <div class="text-center">
-            <h1>QUEM SOMOS?</h1>
-        </div>
-        <img src="imagens/cooking.png" alt="" class="img-rounded img-responsive">
-        <div>
-        <section class="container my-5">
-            <div class="row text-center">
-
-                <div class="col-md-4 mb-4">
-                    <div class="card shadow-sm h-100 border-0">
-                        <div class="card-body">
-                            <h2 class="fw-bold text-success">+12</h2>
-                            <p class="mb-1">Toneladas de alimentos</p>
-                            <small class="text-muted">resgatados do desperdício</small>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-4 mb-4">
-                    <div class="card shadow-sm h-100 border-0">
-                        <div class="card-body">
-                            <h2 class="fw-bold text-success">+1.500</h2>
-                            <p class="mb-1">Famílias atendidas</p>
-                            <small class="text-muted">impactadas diretamente</small>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-4 mb-4">
-                    <div class="card shadow-sm h-100 border-0">
-                        <div class="card-body">
-                        <h2 class="fw-bold text-success">+4.000</h2>
-                        <p class="mb-1">Refeições distribuídas</p>
-                        <small class="text-muted">com dignidade e cuidado</small>
-                    </div>
-                </div>
-            </div>
-        </section>
-</main>
+        
+    </main>
 <!-- Link arquivos Bootstrap js -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="js/bootstrap.min.js"></script>    
