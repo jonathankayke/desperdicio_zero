e401009d3e294b01e2a338883a2ea6fcebc19db7
Mtths_nax <matheusnalesso9@gmail.com>
2026-01-30 19:51:31 -0300

    modified:   admin/menu_adm.php

--- admin/menu_adm.php
+++ admin/menu_adm.php
@@ -27,19 +27,18 @@
                     <span class="icon-bar"></span>
                 </button>
     
-                <a href="index.php" class="navbar-brand">
+                <a href="admin2.php" class="navbar-brand">
                     <img src="imagens/Icon_menu.png" alt="">
                 </a>
             </div>
     
             <div class="collapse navbar-collapse" id="defaultNavbar">
                 <ul class="nav navbar-nav navbar-right">
-                    <li class="active"><a href="index.php">ADMIN</a></li>
-                    <li><a href="doacao_lista.php">DOAÇÕES</a></li>
-                    <li><a href="tipos_lista.php">TIPOS</a></li>   
-                    <li><a href="usuario_lista.php">USUÁRIOS</a></li>
+                    <li><a href="#">DOAÇÕES</a></li>
+                    <li><a href="#">TIPOS</a></li>   
+                    <li><a href="#">USUÁRIOS</a></li>
                     <li class="active">
-                        <a href="../index.php">
+                        <a href="admin2.php">
                             <span class="glyphicon glyphicon-home"></span>
                         </a>
                     </li>
