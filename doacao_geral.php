d3fee8eca44e674dea7f14c79b17e8b7078b6032
jonathankayke <jonathankayke663@gmail.com>
2026-02-02 20:32:48 -0300

    modified:   css/meu_estilo.css

--- doacao_geral.php
+++ doacao_geral.php
@@ -4,7 +4,7 @@ include("Connections/conn_alimentos.php");
 
 // Consulta para trazer os dados
 $consulta = "SELECT * FROM vw_doacoes ORDER BY nome_alimento ASC";
-$lista    = $conn_alimentos->query($consulta);
+$lista = $conn_alimentos->query($consulta);
 
 // Retirei o '$row = ...' daqui para não pular o primeiro item da lista.
 // O 'fetch_assoc' será feito direto no while lá embaixo.
@@ -12,6 +12,7 @@ $totalRows = ($lista)->num_rows;
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -19,70 +20,85 @@ $totalRows = ($lista)->num_rows;
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/meu_estilo.css">
 </head>
+
 <body class="container">
-    
+
     <h2 class="breadcrumb alert-success">Doações</h2>
-    
+
     <div class="row">
-        <?php 
+        <?php
         // Verifica se tem produtos antes de começar o loop
-        if($totalRows > 0) {
-            while ($row = $lista->fetch_assoc()) { 
-        ?>
-        
-        <div class="thumbnail">
-            <div class="row" style="display: flex; align-items: center; flex-wrap: wrap;">
+        if ($totalRows > 0) {
+            while ($row = $lista->fetch_assoc()) {
+                // Gerar um ID único para cada item da lista
+                $collapseID = "detalhes_" . $row['id_doacao'];
+                ?>
 
-                <div class="col-sm-2">
-                    <img 
-                        src="imagens/<?php echo $row['imagem_doacao']; ?>" 
-                        class="img-responsive img-rounded"
-                        style="max-height: 80px; margin: 0 auto; display: block;" 
-                    >
+                <div class="thumbnail" style="margin-bottom: 20px; padding: 10px;">
+                    <div class="row" style="display: flex; align-items: center; flex-wrap: wrap;">
+
+                        <div class="col-sm-2">
+                            <img src="imagens/<?php echo $row['imagem_doacao']; ?>" class="img-responsive img-rounded"
+                                style="max-height: 80px; margin: 0 auto; display: block;">
+                        </div>
+
+                        <div class="col-sm-2">
+                            <h4 class="text-danger text-center">
+                                <strong><?php echo $row['nome_alimento']; ?></strong>
+                            </h4>
+                        </div>
+
+                        <div class="col-sm-2 text-center">
+                            <p><strong>Tipo:</strong><br><?php echo $row['rotulo_tipo']; ?></p>
+                        </div>
+
+                        <div class="col-sm-2 text-center">
+                            <p><strong>Quantidade:</strong><br><?php echo $row['quantidade_doacao']; ?></p>
+                        </div>
+
+                        <div class="col-sm-2 text-center">
+                            <p>
+                                <strong>Validade:</strong><br>
+                                <?php echo date('d/m/Y', strtotime($row['validade_doacao'])); ?>
+                            </p>
+                        </div>
+
+                        <div class="col-sm-2 text-center">
+                            <p><strong>Empresa:</strong><br><?php echo $row['nome_empresa']; ?></p>
+
+                            <button type="button" class="btn btn-success btn-sm" data-toggle="collapse"
+                                data-target="#<?php echo $collapseID; ?>">
+                                Ver detalhes
+                            </button>
+                        </div>
+
+                    </div>
+                    
+                    <div id="<?php echo $collapseID; ?>" class="collapse">
+                        <hr>
+                        <div class="row card" style="padding: 10px; border-radius: 4px; box-shadow: 0 0 15px rgba(0,0,0,0.1) !important;">
+                            <div class="col-sm-4">
+                                <p><strong><i class="glyphicon glyphicon-phone"></i> Contato:</strong><br>
+                                    <?php echo $row['contato_doacao']; ?></p>
+                            </div>
+                            <div class="col-sm-8">
+                                <p><strong><i class="glyphicon glyphicon-map-marker"></i> Endereço de Retirada:</strong><br>
+                                    <?php echo $row['endereco_retirada']; ?></p>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
-         
-                <div class="col-sm-2">
-                    <h4 class="text-danger text-center">
-                        <strong><?php echo $row['nome_alimento']; ?></strong>
-                    </h4>
-                </div>    
-
-                <div class="col-sm-2 text-center">    
-                    <p><strong>Tipo:</strong><br><?php echo $row['rotulo_tipo']; ?></p>
-                </div>
-
-                <div class="col-sm-2 text-center">
-                    <p><strong>Quantidade:</strong><br><?php echo $row['quantidade_doacao']; ?></p>
-                </div>
-
-                <div class="col-sm-2 text-center">
-                    <p>
-                        <strong>Validade:</strong><br>
-                        <?php echo date('d/m/Y', strtotime($row['validade_doacao'])); ?>
-                    </p>
-                </div>
-
-                <div class="col-sm-2 text-center">    
-                    <p><strong>Empresa:</strong><br><?php echo $row['nome_empresa']; ?></p>
-
-                    <a 
-                        href="nome_alimento.php?id_doacao=<?php echo $row['id_doacao']; ?>" 
-                        class="btn btn-success btn-sm dro"
-                    >
-                        Ver detalhes
-                    </a>
-                </div>
-
-            </div> 
-        </div> 
-        <?php 
+            <?php
             } // Fim do While
-        } else { 
-        ?>
+        } else {
+            ?>
             <div class="alert alert-warning">Nenhuma doação encontrada.</div>
         <?php } ?>
-    </div> <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script> 
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 </body>
+
 </html>
 <?php mysqli_free_result($lista); ?>
