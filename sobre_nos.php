c60f581f3232bc2dced590c5aab92dbf09c9bbf6
vinicius <viniciusresende3486@gmail.com>
2026-02-02 21:54:20 -0300

    deleted:    .vscode/launch.json 	modified:   css/meu_estilo.css 	modified:   sobre_nos.php

--- sobre_nos.php
+++ sobre_nos.php
@@ -14,7 +14,21 @@
     <header class=" text-center header-sobrenos">
         <h2 class="h2-sobrenos">Sobre Nós</h2>
     </header>
-    
+    <section class="hero-com-foto">
+        <div class="container container-config">
+            <h1 class="text-center">Transformando Vidas!</h1>
+        </div>
+    </section>
+    <section class="hero-branco">
+        <div class="container container-config">
+            <p>nossa historia aqui</p>
+        </div>
+    </section>
+    <section class="">
+        <div class="container">
+            
+        </div>
+    </section>
 <!-- Link arquivos Bootstrap js -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="../js/bootstrap.min.js"></script>    
