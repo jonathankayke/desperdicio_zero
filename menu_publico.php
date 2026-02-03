03837279605ad90cbab39b8a38c78e63967f89c7
Mtths_nax <matheusnalesso9@gmail.com>
2026-02-02 21:16:50 -0300

    modified:   menu_publico.php

--- menu_publico.php
+++ menu_publico.php
@@ -1,3 +1,17 @@
+<?php
+// Incluir o arquivo para fazer a conexão e buscar os tipos para o menu
+include("Connections/conn_alimentos.php");
+
+$tabela_menu    =   "tbtipos";
+$ordernar_menu  =   "rotulo_tipo";
+$consulta_menu  =   "
+                    SELECT  *
+                    FROM    ".$tabela_menu."\n                    ORDER BY ".$ordernar_menu.";
+                    ";
+$lista_menu     =   $conn_alimentos->query($consulta_menu);
+$row_menu       =   $lista_menu->fetch_assoc();
+$totalRows_menu =   ($lista_menu)->num_rows;
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -75,3 +89,4 @@
 </body>
 
 </html>
+<?php mysqli_free_result($lista_menu); ?>
