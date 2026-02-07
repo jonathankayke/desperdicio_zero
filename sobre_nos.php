fb30f0f44c0d5b63296463843e61490990eae59c
vinicius <viniciusresende3486@gmail.com>
2026-02-06 22:15:28 -0300

    modified:   css/meu_estilo.css 	modified:   sobre_nos.php

--- sobre_nos.php
+++ sobre_nos.php
@@ -1,87 +1,124 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Sobre nós - Desperdício Zero</title>
-    <script src="https://kit.fontawesome.com/9ee3096070.js" crossorigin="anonymous"></script>
+    <script
+      src="https://kit.fontawesome.com/9ee3096070.js"
+      crossorigin="anonymous"
+    ></script>
     <!-- Link CSS do Bootstrap -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
     <!-- Link para CSS Específico -->
-    <link rel="stylesheet" href="css/meu_estilo.css">
-</head>
-<body>
+    <link rel="stylesheet" href="css/meu_estilo.css" />
+  </head>
+  <body>
     <?php include('menu_publico.php')?>
 
     <section class="hero-com-foto">
-        <div class="container container-config">
-            
-        </div>
+      <div class="container container-config"></div>
     </section>
     <section class="hero-branco max-width">
-        <div class="container container-config text-center">
-            <h1>NOSSA HISTORIA!!</h1>
-        </div>
+      <div class="container container-config text-center">
+        <h1>NOSSA HISTORIA!!</h1>
+      </div>
     </section>
     <section class="hero max-width">
-        <div class="container container-config text-center">
-            <h2>Desperdício Zero</h2>
-            <p class="txt-historia">O Desperdício Zero é uma plataforma dedicada a combater o desperdício de alimentos, conectando doadores e beneficiários de forma eficiente e sustentável. Nossa missão é reduzir o impacto ambiental do desperdício alimentar, promovendo a solidariedade e o acesso a alimentos para aqueles que mais precisam. Através de parcerias com supermercados, restaurantes e agricultores, facilitamos a doação de alimentos que ainda estão em boas condições, garantindo que eles cheguem às mãos certas. Junte-se a nós nessa causa nobre e ajude a construir um futuro mais sustentável e justo para todos.</p>
-        </div>
+      <div class="container container-config text-center">
+        <h2>Desperdício Zero</h2>
+        <p class="txt-historia">
+          O Desperdício Zero é uma plataforma dedicada a combater o desperdício
+          de alimentos, conectando doadores e beneficiários de forma eficiente e
+          sustentável. Nossa missão é reduzir o impacto ambiental do desperdício
+          alimentar, promovendo a solidariedade e o acesso a alimentos para
+          aqueles que mais precisam. Através de parcerias com supermercados,
+          restaurantes e agricultores, facilitamos a doação de alimentos que
+          ainda estão em boas condições, garantindo que eles cheguem às mãos
+          certas. Junte-se a nós nessa causa nobre e ajude a construir um futuro
+          mais sustentável e justo para todos.
+        </p>
+      </div>
     </section>
     <section class="hero-verde">
-        <div class="container container-config">
-            <div class="row row-eq-height">
-                <div class="col-xs-12 col-md-4 thumbnail-col">
-                    <div class="thumbnail thumb-equal text-center">
-                        <i class="fa-solid fa-bullseye fa-10x"></i>
-                        <div class="caption">
-                            <h3 class="thumb-title">Missão</h3>
-                            <p style="font-size: 15px;">
-                                Promover a redução do desperdício de alimentos por meio da conexão
-                                entre doadores e beneficiários, incentivando a solidariedade,
-                                a sustentabilidade e o acesso justo à alimentação para todos que precisam.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xs-12 col-md-4 thumbnail-col">
-                    <div class="thumbnail thumb-equal text-center">
-                        <i class="fa-solid fa-eye fa-10x"></i>
-                        <div class="caption">
-                            <h3 class="thumb-title">Visão</h3>
-                            <p style="font-size: 15px;">
-                                Ser uma plataforma de referência no combate ao desperdício de alimentos,
-                                contribuindo para um futuro mais sustentável, onde recursos sejam melhor
-                                aproveitados e nenhuma comida em boas condições seja descartada.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xs-12 col-md-4 thumbnail-col">
-                    <div class="thumbnail thumb-equal text-center">
-                        <i class="fa-solid fa-hand-holding-heart fa-10x"></i>
-                        <div class="caption">
-                            <h3 class="thumb-title">Valores</h3>
-                            <p style="font-size: 15px;">
-                                Solidariedade, responsabilidade social, sustentabilidade,
-                                respeito ao meio ambiente e compromisso com a transparência
-                                e a dignidade de todas as pessoas envolvidas.
-                            </p>
-                        </div>
-                    </div>
-                </div>
+      <div class="container container-config">
+        <div class="row row-eq-height">
+          <div class="col-xs-12 col-md-4 thumbnail-col">
+            <div class="thumbnail thumb-equal text-center">
+              <i class="fa-solid fa-bullseye fa-10x"></i>
+              <div class="caption">
+                <h3 class="thumb-title">Missão</h3>
+                <p style="font-size: 15px">
+                  Promover a redução do desperdício de alimentos por meio da
+                  conexão entre doadores e beneficiários, incentivando a
+                  solidariedade, a sustentabilidade e o acesso justo à
+                  alimentação para todos que precisam.
+                </p>
+              </div>
             </div>
+          </div>
+          <div class="col-xs-12 col-md-4 thumbnail-col">
+            <div class="thumbnail thumb-equal text-center">
+              <i class="fa-solid fa-eye fa-10x"></i>
+              <div class="caption">
+                <h3 class="thumb-title">Visão</h3>
+                <p style="font-size: 15px">
+                  Ser uma plataforma de referência no combate ao desperdício de
+                  alimentos, contribuindo para um futuro mais sustentável, onde
+                  recursos sejam melhor aproveitados e nenhuma comida em boas
+                  condições seja descartada.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-12 col-md-4 thumbnail-col">
+            <div class="thumbnail thumb-equal text-center">
+              <i class="fa-solid fa-hand-holding-heart fa-10x"></i>
+              <div class="caption">
+                <h3 class="thumb-title">Valores</h3>
+                <p style="font-size: 15px">
+                  Solidariedade, responsabilidade social, sustentabilidade,
+                  respeito ao meio ambiente e compromisso com a transparência e
+                  a dignidade de todas as pessoas envolvidas.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </section>
     <section class="hero max-width">
-        <div class="container container-config text-center">
-            <h2>Nosso impacto</h2>
-            <p class="txt-historia">Resultados concretos no combate ao desperdício de alimentos</p>
+      <div class="container container-config text-center">
+        <h2>Nosso impacto</h2>
+        <p class="txt-historia">
+          Resultados concretos no combate ao desperdício de alimentos
+        </p>
+      </div>
+      <div class="container-fluid hero-card">
+        <div class="card-stat">
+          <i class="fa-solid fa-utensils fa-3x"></i>
+          <h2 style="font-weight: bolder">+12t</h2>
+          <p style="font-size: 17px">Alimentos Reaproveitados.</p>
         </div>
+        <div class="card-stat">
+          <i class="fa-solid fa-users fa-3x"></i>
+          <h2 style="font-weight: bolder">+8.000</h2>
+          <p style="font-size: 17px">Famílias Beneficiadas.</p>
+        </div>
+        <div class="card-stat">
+          <i class="fa-solid fa-store fa-3x"></i>
+          <h2 style="font-weight: bolder">120</h2>
+          <p style="font-size: 17px">Colaboradores Ativos.</p>
+        </div>
+        <div class="card-stat">
+          <i class="fa-solid fa-recycle fa-3x"></i>
+          <h2 style="font-weight: bolder">65%</h2>
+          <p style="font-size: 17px">Redução do Desperdício.</p>
+        </div>
+      </div>
     </section>
-<!-- Link arquivos Bootstrap js -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="../js/bootstrap.min.js"></script>    
-</body>
+    <!-- Link arquivos Bootstrap js -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+  </body>
 </html>
