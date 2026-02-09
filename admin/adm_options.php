278c16d95e20fd02feafdec2af7ae825f0e0f0b8
Mtths_nax <matheusnalesso9@gmail.com>
2026-02-09 20:51:53 -0300

    modified:   admin/adm_options.php 	modified:   css/meu_estilo.css

--- admin/adm_options.php
+++ admin/adm_options.php
@@ -8,15 +8,20 @@
     <script src="https://kit.fontawesome.com/9ee3096070.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/meu_estilo.css">
+  
 </head>
 
 <body class="fundofixo">
     <main class="container">
-        <h1 class="breadcrumb text-success">Área Administrativa</h1>
+        <h1 class="breadcrumb text-success text-center">Área Administrativa</h1>
         <div class="row">
             
             <div class="col-sm-6 col-md-4">
-                <div class="thumbnail doacoes-card fa">
+                <div class="thumbnail doacoes-card text-center">
+                  <span class="fa-7x">
+                    <i class="fa-solid fa-bag-shopping" style="color: #28A745;"></i>
+                  </span>
+            
                     <br>
                     <div class="btn-group btn-group-justified" role="group">
                         <div class="btn-group">
@@ -41,9 +46,14 @@
             </div>
 
             <div class="col-sm-6 col-md-4">
-                <div class="thumbnail" style="background-color: #FCF8E3;">
+                <div class="thumbnail text-center" style="background-color: #FCF8E3;">
+                    <span class="fa-7x">
+                        <i class="fa-solid fa-layer-group" style="color: #ffab45;"></i>
+                    </span>
+                    
                     <br>
                     <div class="btn-group btn-group-justified" role="group">
+                        
                         <div class="btn-group">
                             <div class="fundo-branco">
                                 <span>TIPOS</span>
@@ -66,7 +76,10 @@
             </div>
 
             <div class="col-sm-6 col-md-4">
-                <div class="thumbnail " style="background-color: #c9e9f8ff;">
+                <div class="thumbnail text-center" style="background-color: #c9e9f8ff;">
+                    <span class="fa-7x">
+                        <i class="fa-solid fa-user-pen" style="color: #007BFF;"></i>
+                    </span>
                     <br>
                     <div class="btn-group btn-group-justified" role="group">
                         <div class="btn-group">
