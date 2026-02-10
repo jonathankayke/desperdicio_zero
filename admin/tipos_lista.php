f59f1dac602f59f4e095c1ad4ac79e318f960312
gustavoalmeidacamargo16-maker <gustavoalmeidacamargo16@gmail.com>
2026-02-10 20:13:58 -0300

    new file:   admin/acesso_com.php 	modified:   admin/doacao_lista.php 	modified:   admin/indexadmin.php 	modified:   admin/invasor.php 	new file:   admin/invasor_user.php 	modified:   admin/login.php 	new file:   admin/logout.php 	modified:   admin/tipos_lista.php 	modified:   admin/usuario_lista.php

--- admin/tipos_lista.php
+++ admin/tipos_lista.php
@@ -1,4 +1,7 @@
 <?php
+// Incluindo o Sistema de autenticação
+include("acesso_com.php");
+
 // Incluir o arquivo e fazer a conexão
 include("../Connections/conn_alimentos.php");
 // Selecionar os dados
