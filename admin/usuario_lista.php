f332e13f7e0bfdc04a347ba8521c45d134d699f1
gustavoalmeidacamargo16-maker <gustavoalmeidacamargo16@gmail.com>
2026-02-13 20:12:12 -0300

    modified:   admin/acesso_admin.php 	modified:   admin/usuario_lista.php

--- admin/usuario_lista.php
+++ admin/usuario_lista.php
@@ -49,11 +49,13 @@ $totalRows  =   $lista->num_rows;
                     </tr>
                 </thead>
                 <tbody>
+                
                     <?php
                     // O while verifica se existe uma linha antes de tentar imprimir
-                    while ($row = $lista->fetch_assoc()) {
+                    while ($row = $lista->fetch_assoc()) {;
                     ?>
                         <tr>
+                        
                             <td>
                                 <img src="../imagens/<?php echo $row['foto_usuario']; ?>" alt="<?php echo $row['nome_usuario']; ?>" class="img-responsive img-thumbnail" style="max-width: 80px;">
                             </td> <td><?php echo $row['nome_usuario']; ?></td>
