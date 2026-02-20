838f293e4ddfc883d0eaea05b7413776b1c66bdb
Mtths_nax <matheusnalesso9@gmail.com>
2026-02-19 21:09:38 -0300

    modified:   admin/menu_adm.php

--- admin/menu_adm.php
+++ admin/menu_adm.php
@@ -33,8 +33,8 @@
             <ul class="nav navbar-nav navbar-right borda-preta">
 
                 <li>
-                    <button type="button" class="btn btn-danger navbar-btn disabled">
-                        Olá, <?php echo($_SESSION['login_usuario']); ?>
+                    <button type="button" class="btn btn-secondary navbar-btn disabled">
+                        <span>Olá, <?php echo($_SESSION['login_usuario']); ?></span>
                     </button>
                 </li>
 
