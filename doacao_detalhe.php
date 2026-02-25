7323441f1cce4a556d404cb8c3dd842c621d4d13
Mtths_nax <matheusnalesso9@gmail.com>
2026-02-24 21:31:09 -0300

    modified:   doacao_detalhe.php

--- doacao_detalhe.php
+++ doacao_detalhe.php
@@ -113,7 +113,7 @@ $totalRows = $lista->num_rows;
     <!-- Link arquivos Bootstrap js -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
-<?php include("rodape.php"); ?>
+
 </body>
 
 
