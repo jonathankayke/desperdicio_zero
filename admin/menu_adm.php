d70a1b9a7733dc4cd86b1195177443aab856e10b
Mtths_nax <matheusnalesso9@gmail.com>
2026-02-24 21:42:49 -0300

    modified:   admin/menu_adm.php

--- admin/menu_adm.php
+++ admin/menu_adm.php
@@ -7,8 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Área Administrativa</title>
     
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/meu_estilo.css">
+  
 </head>
 
 <body>
@@ -66,8 +65,7 @@
         </div>
     </nav>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 </body>
 
 </html>
